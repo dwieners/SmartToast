f27dbf40123bde7943255a39b27fd4180336fcbf
Dominik Wieners <dom.wieners@gmx.net>
2018-10-03 18:57:01 +0200

    Add first Toast implementation

--- smarttoast/src/main/java/de/dominikwieners/smarttoast/SmartToast.kt
+++ smarttoast/src/main/java/de/dominikwieners/smarttoast/SmartToast.kt
@@ -1,5 +1,16 @@
 package de.dominikwieners.smarttoast
 
+import android.content.Context
+import android.widget.Toast
+
 class SmartToast{
+
+    fun normal(context:Context, message:Int):Toast{
+        return normal(context, message, Toast.LENGTH_LONG)
+    }
+
+    fun normal(context: Context, message: Int, duration:Int):Toast{
+        return Toast.makeText(context, context.getString(message), duration)
+    }
 
 }
