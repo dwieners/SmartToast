91b61377761342c923af80bef8e61728b4a6b558
Dominik Wieners <dom.wieners@gmx.net>
2018-10-04 20:58:04 +0200

    close #1

--- smarttoast/src/main/java/de/dominikwieners/smarttoast/SmartToast.kt
+++ smarttoast/src/main/java/de/dominikwieners/smarttoast/SmartToast.kt
@@ -24,43 +24,38 @@ class SmartToast{
         val ERROR_COLOR = Color.parseColor("#e53935")
         val INFO_COLOR = Color.parseColor("#f9a825")
         val SUCCESS_COLOR = Color.parseColor("#008577")
+        val DEFAULT_TEXT_COLOR = Color.parseColor("#FFFFFF")
 
         fun normal(@NonNull context:Context, @StringRes message:CharSequence):Toast{
             return normal(context, message, Toast.LENGTH_LONG)
         }
 
         fun normal(@NonNull context: Context, @StringRes message: CharSequence, @NonNull duration:Int):Toast{
-            return Toast.makeText(context, message, duration)
+            return Toast.makeText(context, DEFAULT_TEXT_COLOR, duration)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun success(@NonNull context: Context, @StringRes message: CharSequence, @NonNull duration: Int):Toast{
-            return custom(context, message, context.getColor(android.R.color.white) , SUCCESS_COLOR, duration)
+            return custom(context, message, DEFAULT_TEXT_COLOR , SUCCESS_COLOR, duration)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun success(@NonNull context: Context, @StringRes message: CharSequence):Toast{
-            return custom(context, message, context.getColor(android.R.color.white), SUCCESS_COLOR, Toast.LENGTH_LONG)
+            return custom(context, message, DEFAULT_TEXT_COLOR, SUCCESS_COLOR, Toast.LENGTH_LONG)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun info(@NonNull context: Context, @StringRes message: CharSequence, @NonNull duration: Int):Toast{
-            return custom(context, message, context.getColor(android.R.color.white), INFO_COLOR, duration)
+            return custom(context, message, DEFAULT_TEXT_COLOR, INFO_COLOR, duration)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun info(@NonNull context: Context, @StringRes message: CharSequence):Toast{
-            return custom(context, message, context.getColor(android.R.color.white), INFO_COLOR, Toast.LENGTH_LONG)
+            return custom(context, message, DEFAULT_TEXT_COLOR, INFO_COLOR, Toast.LENGTH_LONG)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun error(@NonNull context: Context, @StringRes message: CharSequence, @NonNull duration: Int):Toast{
-            return custom(context, message, context.getColor(android.R.color.white), ERROR_COLOR, duration)
+            return custom(context, message, DEFAULT_TEXT_COLOR, ERROR_COLOR, duration)
         }
 
-        @TargetApi(Build.VERSION_CODES.M)
         fun error(@NonNull context: Context, @StringRes message: CharSequence):Toast{
-            return custom(context, message, context.getColor(android.R.color.white), ERROR_COLOR, Toast.LENGTH_LONG)
+            return custom(context, message, DEFAULT_TEXT_COLOR, ERROR_COLOR, Toast.LENGTH_LONG)
         }
 
         fun custom(@NonNull context: Context, @StringRes message: CharSequence, @ColorInt textColor:Int, @ColorInt toastColor:Int, @NonNull duration:Int) : Toast{
@@ -70,7 +65,6 @@ class SmartToast{
             val toastTextView = toastLayout.findViewById<TextView>(R.id.toast_text)
             val root:View = toastLayout.findViewById(R.id.toast_root)
 
-            root.background = getDrawable(context, android.R.drawable.toast_frame)
             root.background.setColorFilter(toastColor, PorterDuff.Mode.SRC_IN)
             toastTextView.setTextColor(textColor)
 
